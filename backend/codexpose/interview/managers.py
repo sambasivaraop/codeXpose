6fd54bc825611fd58da5bea7d78c5e5cd89d5368
sivap <sambasiva1210@gmail.com>
2018-05-23 14:58:41 +0530

    Change for improving code coverage
    Added few tests in existing files
    
    Added new file test_managers.py
    
    Excluded admin.py and app.py from coverage through .coveragerc file

--- backend/codexpose/interview/managers.py
+++ backend/codexpose/interview/managers.py
@@ -3,15 +3,11 @@ from django.contrib.auth.models import BaseUserManager
 
 
 class UserManager(BaseUserManager):
-    """
-    User manager class to handle the creation of users.
-    """
+    """User manager class to handle the creation of users."""
     use_in_migrations = True
 
     def _create_user(self, email, password):
-        """
-        Creates and saves a User with the given email and password.
-        """
+        """Creates and saves a User with given email and password."""
         if not email:
             raise ValueError('The given email must be set')
         email = self.normalize_email(email)
--- backend/codexpose/interview/tests/test_models.py
+++ backend/codexpose/interview/tests/test_models.py
@@ -1,31 +1,35 @@
 """The module contains all the test cases for the project"""
 
+import configparser
 from datetime import datetime
 from django.test import TestCase
 from django.core.files import File
 from ..models import User, Question, Test, CandidateTestMapping, \
-    CandidateResult, CandidateSolution
+    CandidateResult, CandidateSolution, candidate_solution_data_path
 
 PATH = "interview/tests/test.txt"
+CONFIG_PATH = "interview/tests/config.ini"
 
 
-class CodexposeModelTestCase(TestCase):
+class ModelTestCase(TestCase):
     """The class contains test cases for hrbot."""
 
-    @classmethod
-    def setUpClass(cls):
-        """setUpClass is used instead of setUp
-        to avoid multiple creation of objects."""
-        cls.user_obj = User.objects.create(
-            first_name='akshat',
-            last_name='goel',
-            email='test.abc@xyz.com',
-            user_type='INTERVIEWER'
+    def setUp(self):
+        """setUp is used instead for creation of objects."""
+        # Class method is getting skipped in the execution.
+        # Hence changed to instance method.
+        self.config = configparser.ConfigParser()
+        self.config.read(CONFIG_PATH)
+        self.user_obj = User.objects.create(
+            first_name=self.config['user']['first_name'],
+            last_name=self.config['user']['last_name'],
+            email=self.config['user']['email'],
+            user_type=self.config['user']['user_type']
         )
-        cls.ques_obj = Question.objects.create(
-            question_id=1,
-            title='Python',
-            question_type='Python',
+        self.ques_obj = Question.objects.create(
+            question_id=self.config['question']['question_id'],
+            title=self.config['question']['title'],
+            question_type=self.config['question']['question_type'],
             problem_statement=File(
                 open(PATH)
             ),
@@ -37,56 +41,58 @@ class CodexposeModelTestCase(TestCase):
             ),
             marks=10
         )
-        cls.test_obj = Test.objects.create(
-            title='Python Test',
-            created_by=cls.user_obj
+        self.test_obj = Test.objects.create(
+            title=self.config['test']['title'],
+            created_by=self.user_obj
         )
-        cls.test_obj.question.add(cls.ques_obj)
+        self.test_obj.question.add(self.ques_obj)
 
-        cls.candidate_test_mapping_obj = CandidateTestMapping.objects.create(
-            candidate=cls.user_obj,
-            test=cls.test_obj,
+        self.candidate_test_mapping_obj = CandidateTestMapping.objects.create(
+            candidate=self.user_obj,
+            test=self.test_obj,
             schedule=datetime.now()
         )
-        cls.candidate_result = CandidateResult.objects.get(
-            candidate_test_mapping=cls.candidate_test_mapping_obj
+        self.candidate_result = CandidateResult.objects.get(
+            candidate_test_mapping=self.candidate_test_mapping_obj
         )
-        cls.candidate_solution = CandidateSolution.objects.create(
-            candidate_result=cls.candidate_result,
-            question=cls.ques_obj
+        self.candidate_solution = CandidateSolution.objects.create(
+            candidate_result=self.candidate_result,
+            question=self.ques_obj
         )
 
-    @classmethod
-    def tearDownClass(cls):
-        """tearDownClass is used instead of tearDown
-        to avoid multiple deletion of objects."""
-        del cls.user_obj
-        del cls.ques_obj
-        del cls.test_obj
-        del cls.candidate_test_mapping_obj
-        del cls.candidate_result
-        del cls.candidate_solution
+    def tearDown(self):
+        """tearDown is used for deletion of objects."""
+        # Class method is getting skipped in the execution.
+        # Hence changed to instance method.
+        del self.config
+        del self.user_obj
+        del self.ques_obj
+        del self.test_obj
+        del self.candidate_test_mapping_obj
+        del self.candidate_result
+        del self.candidate_solution
 
     def test_user(self):
-        """Test case for User model"""
-
+        """Test case for User model."""
         self.assertTrue(isinstance(self.user_obj, User), 'User Object')
         self.assertEqual(self.user_obj.__str__(), self.user_obj.email)
+        self.assertTrue(self.user_obj.has_perm('edit'))
+        self.assertTrue(self.user_obj.has_module_perms('interview'))
 
     def test_question(self):
-        """Test case for Question model"""
+        """Test case for Question model."""
 
         self.assertTrue(isinstance(self.ques_obj, Question), 'Question Object')
         self.assertEqual(self.ques_obj.__str__(), self.ques_obj.title)
 
     def test_test(self):
-        """Test case for Test model"""
+        """Test case for Test model."""
 
         self.assertTrue(isinstance(self.test_obj, Test), 'Test Object')
         self.assertEqual(self.test_obj.__str__(), self.test_obj.title)
 
     def test_candidate_test_mapping(self):
-        """Test case for CandidateTestMapping model"""
+        """Test case for CandidateTestMapping model."""
 
         self.assertTrue(isinstance(
             self.candidate_test_mapping_obj,
@@ -96,7 +102,7 @@ class CodexposeModelTestCase(TestCase):
             self.user_obj.email + " : " + self.test_obj.title)
 
     def test_candidate_result(self):
-        """Test case for Candidate Result model"""
+        """Test case for Candidate Result model."""
 
         self.assertTrue(isinstance(
             self.candidate_result,
@@ -106,8 +112,17 @@ class CodexposeModelTestCase(TestCase):
             self.candidate_test_mapping_obj.__str__())
 
     def test_candidate_solution(self):
-        """Test case CandidateSolution model"""
+        """Test case CandidateSolution model."""
 
         self.assertTrue(isinstance(
             self.candidate_solution,
             CandidateSolution), 'Candidate Solution Object')
+
+    def test_solution_data_path(self):
+        """Test case candidate solution data path."""
+        self.assertEqual(
+            candidate_solution_data_path(self.candidate_solution, "test.txt"),
+            'ct_{0}/question_{1}/test.txt'.format(
+                self.candidate_solution
+                .candidate_result.candidate_test_mapping.id,
+                self.candidate_solution.question.id))
